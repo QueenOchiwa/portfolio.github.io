07822c5e5bd1b60d77c08d3a2ed79c662c49b9cb
Larose <86922499+QueenOchiwa@users.noreply.github.com>
2022-09-11 17:59:12 +0100

    Update message.php

--- message.php
+++ message.php
@@ -1,17 +1,24 @@
 <?php
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
     //lets get all our values
+    $headers = array("From: from@example.com",
+        "Reply-To: replyto@example.com",
+        "X-Mailer: PHP/" . PHP_VERSION
+    );
+    $headers = implode("\r\n", $headers);
+    
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
     if(!empty($email) && !empty($message)) {
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $receiver = "emailme@larosedev.site"; //email receiver adress
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) { //email receiver adress
             $subject = "From $name <$email>";
             $body = "Name: $name\nEmail: $email\n\nMessage: $message\n\nRegards,\n$name";
-            $sender = "From: $email"; //sender email
 
-            if(mail($receiver, $subject, $body, $sender)) {// mail() is an inbuilt function
+            if(mail('emailme@larosedev.site', $subject, $body, $headers)) {// mail() is an inbuilt function
                 echo "Your Message has been sent! ✔";
             } else {
                 echo "Sorry, failed to send your message!";
